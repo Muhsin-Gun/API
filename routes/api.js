40353913f10b327dd36b9d72075e41aa8eccf331
Muhsin-Gun <muhsinabdi288@gmail.com>
2025-06-17 11:37:37 +0300

    student control

--- routes/api.js
+++ routes/api.js
@@ -1,53 +1,19 @@
-
 const express = require("express");
 const routes = express.Router();
-const Student = require("../models/studentmodels");
+const studentController = require("../Controller/studentController");
 
 // Get a list of students from the database
-routes.get('/students', (req, res) => {
-    res.send({ type: 'Get Request' });
-});
+routes.get('/getstudent', studentController.getAllStudents);
 
 // Add a student to the database
-routes.post('/students', async (req, res, next) => {
-    try {
-        const student = new Student(req.body);
-        const result = await student.save();
-        res.send(result);
-    } catch (error) {
-        console.log(error.message);
-       
-    }
-});
+routes.post('/addstudent', studentController.addStudent);
 
 // Update a student in the database
-routes.put('/students/:id', (req, res) => {
-    res.send({ type: 'Update Request' });
-});
+routes.patch('/updatestudent/:id', studentController.updateStudent);
 
 // Delete a student from the database
-routes.delete('/students/:id', async(req, res) => {
-    const id = req.params.id
-    try {
-        const student = await Student.findByIdAndDelete(id) 
-        res.send(student);
-    } catch (error) {
-        console.log(error.message);
-    }
-});
-routes.patch('/students/:id', async (req, res, next)=> {
-    try {
-        const id = req.params.id;
-        const update = req.body;
-        const options ={new: true}
-        const result = await Student.findByIdAndUpdate(id, update, options)
+routes.delete('/deletestudent/:id', studentController.deleteStudent);
 
-        res.send(result);
-
-    } catch (error) {
-        console.log(error.message)
-    }
-});
 module.exports = routes;
 
 
